cb1c3761ad7456088dd37bd6b4b9daba1f5b4ca7
Thibault Seisel <seisel.thibault@gmail.com>
2022-12-17 12:11:23 +0100

    refactor: rewrite webpack config in TypeScript
    It seems that NestJS supports running webpack with ts-node out of the box.

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,6 +1,4 @@
-/**
- * @typedef {import("webpack").Configuration} WebpackConfiguration
- */
+import { type Configuration as WebpackConfiguration } from "webpack"
 
 /**
  * Whether the built application targets a production environment.
@@ -11,12 +9,11 @@ const isProduction = process.env.NODE_ENV === "production"
  * Configure bundling of applications and libraries with Webpack.
  * This function is intended to be called by the NestJS CLI.
  *
- * @param {WebpackConfiguration} defaults Default configuration produced by the
- * NestJS CLI.
- * @param {import("webpack")} webpack Whole webpack module.
- * @returns {WebpackConfiguration} Webpack configuration to be used.
+ * @param defaults Default configuration produced by the NestJS CLI.
+ * @param webpack Whole webpack module.
+ * @returns Webpack configuration to be used.
  */
-module.exports = (defaults) => ({
+export default (defaults: WebpackConfiguration): WebpackConfiguration => ({
   ...defaults,
   mode: isProduction ? "production" : "development",
   devtool: isProduction ? "source-map" : defaults.devtool,
